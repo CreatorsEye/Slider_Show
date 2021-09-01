bd0e838814ee2c93a8626f98429032a13c92bfe5
cheateye <38386441+cheateye@users.noreply.github.com>
2021-09-01 10:01:15 +0100

    SliderShow Project

---
+++ GC_Panel/GC_Panel/SliderShow.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace GC_Panel
+{
+    partial class SliderShow
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.Next = new System.Windows.Forms.Button();
+            this.Back = new System.Windows.Forms.Button();
+            this.Wallpapers = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Wallpapers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackgroundImage = global::GC_Panel.Properties.Resources.Black_Wall;
+            this.tableLayoutPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+            this.tableLayoutPanel1.Controls.Add(this.Next, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.Back, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.Wallpapers, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(486, 220);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // Next
+            // 
+            this.Next.BackColor = System.Drawing.Color.Transparent;
+            this.Next.BackgroundImage = global::GC_Panel.Properties.Resources.Next;
+            this.Next.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Next.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Next.FlatAppearance.BorderSize = 0;
+            this.Next.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Next.Location = new System.Drawing.Point(454, 38);
+            this.Next.Name = "Next";
+            this.Next.Size = new System.Drawing.Size(29, 144);
+            this.Next.TabIndex = 0;
+            this.Next.UseVisualStyleBackColor = false;
+            this.Next.Click += new System.EventHandler(this.Next_Click);
+            // 
+            // Back
+            // 
+            this.Back.BackColor = System.Drawing.Color.Transparent;
+            this.Back.BackgroundImage = global::GC_Panel.Properties.Resources.back;
+            this.Back.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Back.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Back.FlatAppearance.BorderSize = 0;
+            this.Back.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Back.Location = new System.Drawing.Point(3, 38);
+            this.Back.Name = "Back";
+            this.Back.Size = new System.Drawing.Size(29, 144);
+            this.Back.TabIndex = 1;
+            this.Back.UseVisualStyleBackColor = false;
+            this.Back.Click += new System.EventHandler(this.Back_Click);
+            // 
+            // Wallpapers
+            // 
+            this.Wallpapers.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Wallpapers.Image = global::GC_Panel.Properties.Resources._1;
+            this.Wallpapers.Location = new System.Drawing.Point(38, 38);
+            this.Wallpapers.Name = "Wallpapers";
+            this.Wallpapers.Size = new System.Drawing.Size(410, 144);
+            this.Wallpapers.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Wallpapers.TabIndex = 2;
+            this.Wallpapers.TabStop = false;
+            // 
+            // SliderShow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "SliderShow";
+            this.Size = new System.Drawing.Size(486, 220);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Wallpapers)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button Next;
+        private System.Windows.Forms.Button Back;
+        private System.Windows.Forms.PictureBox Wallpapers;
+    }
+}
---
+++ GC_Panel/GC_Panel/SliderShow.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GC_Panel
+{
+    public partial class SliderShow : UserControl
+    {
+        // Time to code
+        private Bitmap image1 = GC_Panel.Properties.Resources._1;
+        // image1 = image 1 in resources as sortcut of call
+        // GC_Panel are my namespace user yours
+        private Bitmap image2 = GC_Panel.Properties.Resources._2;
+        // image2 = image 2 in resources as sortcut of call
+        private Bitmap image3 = GC_Panel.Properties.Resources._3;
+        // image3 = image 3 in resources as sortcut of call
+        private Bitmap image4 = GC_Panel.Properties.Resources._4;
+        // image4 = image 4 in resources as sortcut of call
+
+
+        public SliderShow()
+        {
+            InitializeComponent();
+        }
+
+        private void Next_Click(object sender, EventArgs e)
+        {
+            // call order now
+            if (Wallpapers.Image == image1)
+            {
+                Wallpapers.Image = image2;
+                // change image from image 1 into image2
+            }
+            else if (Wallpapers.Image == image2)
+            {
+                Wallpapers.Image = image3;
+                // change image from image 2 into image3
+            }
+            else if (Wallpapers.Image == image3)
+            {
+                Wallpapers.Image = image4;
+                // change image from image 3 into image4
+            }
+            else
+            {
+                Wallpapers.Image = image1;
+                // back into first state image1
+            }
+        }
+
+        private void Back_Click(object sender, EventArgs e)
+        {
+            // call order now
+            if (Wallpapers.Image == image1)
+            {
+                Wallpapers.Image = image4;
+                // change image from image 1 into image4
+            }
+            else if (Wallpapers.Image == image4)
+            {
+                Wallpapers.Image = image3;
+                // change image from image 4 into image3
+            }
+            else if (Wallpapers.Image == image3)
+            {
+                Wallpapers.Image = image2;
+                // change image from image 3 into image2
+            }
+            else
+            {
+                Wallpapers.Image = image1;
+                // back into first state image1
+            }
+        }
+    }
+}
